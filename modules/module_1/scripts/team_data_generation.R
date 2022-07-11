07f70cfe28f27d7f925d15af4bc89e115d546e20
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2022-07-11 14:36:57 -0700

    Update and recreate mission applications

--- modules/module_1/scripts/team_data_generation.R
+++ modules/module_1/scripts/team_data_generation.R
@@ -2,35 +2,45 @@
 # Keaton Wilson
 # keatonwilson@me.com
 # 2019-10-28
+# EKB edits, July 2022
 
 # packages
 library(tidyverse)
 library(randomNames)
 library(stringr)
 
-# gender
-gender = sample(0:1, 1000, replace = TRUE)
-gender_labels = ifelse(gender == 0, "M", "F")
+# sex
+sex <- sample(0:1, 1000, replace = TRUE)
+sex[sample(length(sex), 23)] <- 2 # include 23 intersex
+labels <- if_else(sex == 0, "F",
+                 if_else(sex == 1, "M", "I"))
 
 # name generation
-names = randomNames(n = 1000, gender = gender)
-last = unlist(lapply(str_split(names, ","), "[[", 1))
-first = unlist(lapply(str_split(names, ","), "[[", 2))
+names <- as_tibble(randomNames(n = 1000, gender = sex, name.sep = ",")) %>% 
+  rename(name = value) %>% 
+  separate(name, into = c("last", "first"), 
+           sep = ",", remove = TRUE, extra =  "merge", fill = "right")
 
 # ages
-age = sample(seq(19:87), size = 1000, replace = TRUE)
+age <- sample(18:90, size = 1000, replace = TRUE)
 
-# specialty/discpline
+# specialty/discipline
 specialty = c("Mechanical Engineering", "Horticulure", "Aquaculture", "Computer Science", 
-              "Data Science", "Geology", "Marine Biology", "Climatology", 
-              "Electrical Engineering", "Chemical Engineering", "Medicine", 
+              "Data Science", "Geology", "Marine Biology", "Climatology", "Anthropology",
+              "Electrical Engineering", "Chemical Engineering", "Medicine", "Psychology",
               "Management", "Applied Bioscience", "Genetics", "Hydrology", "Food Science")
-
-specialties = sample(specialty, size = 1000, replace = TRUE)
+specialties <- sample(specialty, size = 1000, replace = TRUE)
 
 # weight
-weights = sample(seq(from = 90.0, to = 250, by = 0.5), size = 1000, replace = TRUE)
+weight = sample(seq(from = 90.0, to = 250, by = 0.5), size = 1000, replace = TRUE)
 
-mission_app = data.frame(last, first, gender = gender_labels, age, weight = weights, specialties)
+# height?
+
+# combine all
+mission_app <- data.frame(names, 
+                          sex = labels, 
+                          age, 
+                          weight,
+                          specialties)
 
 write_csv(mission_app, "./modules/module_1/data/mission_team_data.csv")
