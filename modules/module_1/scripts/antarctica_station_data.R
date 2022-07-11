69f2e60e39710fbaa36966ee59e92ac638b81910
Ellen Bledsoe <ellen.bledsoe@weecology.org>
2022-07-11 13:56:50 -0700

    recreate antarctica weather station data csv
    remove empty columns (read_delim -> read_table) and resave csv so it doesn't deal with git lfs, hopefully

--- modules/module_1/scripts/antarctica_station_data.R
+++ modules/module_1/scripts/antarctica_station_data.R
@@ -2,45 +2,35 @@
 # Keaton Wilson
 # keatonwilson@me.com
 # 2019-11-7
+# EKB edits, July 2022
 
 # packages
 library(tidyverse)
-library(lubridate)
 
+# aggregate weather data from all stations
 
-# Multiple stations
-test = read_delim("./modules/module_1/data/raw_antarctica_station_data/ag4201801q3h.txt", 
-                  delim = " ", 
-                  skip = 2, 
-                  col_names = c("year", "day", "month", 
-                                "running_day", "hour", 
-                                "temp", "pressure", 
-                                "wind_speed", "wind_direction", 
-                                "humidity", "delta_t"))
+all_station_data <- data.frame()
 
-test %>%
-  mutate(date = paste(test$year, test$month, test$day, sep = "-") %>% ymd()) %>%
-  mutate_if(is.character, as.numeric)
-
-
-master_station_data = data.frame()
-files = list.files("./modules/module_1/data/raw_antarctica_station_data/", 
+files <- list.files("./modules/module_1/data/raw_antarctica_station_data/", 
                    full.names = TRUE)
+
 for(i in 1:length(files)) {
-  temp = read_delim(files[i], 
-                    delim = " ", 
+  
+  temp <- read_table(files[i], 
                     skip = 2, 
                     col_names = c("year", "day", "month", 
                                   "running_day", "hour", 
                                   "temp", "pressure", 
                                   "wind_speed", "wind_direction", 
                                   "humidity", "delta_t"))
-  temp = temp %>%
-    mutate(date = paste(year, month, day, sep = "-") %>% ymd()) %>%
+  
+  temp <- temp %>%
     mutate_if(is.character, as.numeric) %>%
     mutate(station_id = str_remove(str_sub(files[i], start = -16), 
                                    pattern = ".txt"))
-  master_station_data = bind_rows(master_station_data, temp)
+  
+  all_station_data = bind_rows(all_station_data, temp)
+  
 }
 
-write_csv(master_station_data, "./modules/module_1/data/aggregated_station_data.csv")
+write_csv(all_station_data, "./modules/module_1/data/aggregated_station_data.csv")
